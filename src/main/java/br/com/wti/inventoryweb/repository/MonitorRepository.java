6662a1560a44b7a4c159df0cf95756baf7eed641
Washington Antunes <washington.cead@gmail.com>
2024-04-28 19:46:43 -0300

    refactor: ajustando anotação

---
+++ src/main/java/br/com/wti/inventoryweb/repository/MonitorRepository.java
@@ -0,0 +1,9 @@
+package br.com.wti.inventoryweb.repository;
+
+import br.com.wti.inventoryweb.domain.model.Monitor;
+
+/**
+ * @author Washington Antunes for wTI on 26/04/2024
+ */
+public interface MonitorRepository extends JpaSpecificationRepository<Monitor, Long> {
+}
